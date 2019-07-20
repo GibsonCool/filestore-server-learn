21cd0780e9150285878d2084e4de7085757ceb76
DoubleX <double@tg10010.com>
2019-07-20 16:13:07 +0700

    add 增加 redis 连接池的创建

---
+++ cache/redis/conn.go
@@ -0,0 +1,53 @@
+package redis
+
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"time"
+)
+
+var (
+	pool      *redis.Pool
+	redisHost = "127.0.0.6379"
+	redisPass = ""
+)
+
+// newRedisPool:创建 Redis 链接池
+func newRedisPool() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     50,                //最大链接数量
+		MaxActive:   30,                //最大活跃连接数量
+		IdleTimeout: 300 * time.Second, //超时关闭时间
+		Dial: func() (conn redis.Conn, e error) {
+			//1.打开连接
+			c, e := redis.Dial("tcp", redisHost)
+			if e != nil {
+				fmt.Println(e.Error())
+				return nil, e
+			}
+			//2.访问认证
+			if _, e = c.Do("AUTH", redisPass); e != nil {
+				c.Close()
+				return nil, e
+			}
+			return c, e
+		},
+		// 每分钟去检测一下 redis 链接状况.如果出错了 redis 会自动关闭 shutdown
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+//初始化
+func init() {
+	pool = newRedisPool()
+}
+
+func RedisPool() *redis.Pool {
+	return pool
+}
